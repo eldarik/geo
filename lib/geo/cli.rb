42220fc9542b6fd070afadf2becab31a4ad98a80
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-02-16 16:07:26 +0700

    fix cli

--- lib/geo/cli.rb
+++ lib/geo/cli.rb
@@ -2,8 +2,8 @@ require 'geo'
 
 module Geo
   module CLI
-    def self.start(ip)
-      geo_data = ::Geo.get_data_by_ip(ip)
+    def self.start(ip, http_client = OpenURI)
+      geo_data = ::Geo.get_data_by_ip(ip, http_client)
       Geo::Printer::CLI.new(geo_data).print
     end
   end
--- spec/geo/cli_spec.rb
+++ spec/geo/cli_spec.rb
@@ -1,34 +1,21 @@
 require 'geo/cli'
 
 describe Geo::CLI do
-  let!(:data) do
-    Geo::Data.new(
-      {
-        city: 'City',
-        country: 'Country',
-        lat: '56.5',
-        lon: '56.6',
-        query: '127.0.0.1'
-      }
-    )
-  end
+  let!(:http_client) { TestHTTPClient }
 
   describe 'class methods' do
     describe '.start' do
-      subject { described_class.start(nil) }
-      before do
-        expect(Geo).to receive(:get_data).and_return(data)
-      end
+      subject { described_class.start(nil, http_client) }
 
       it 'print formatted string to stdout' do
         expect { subject }.to output(
           <<~HEREDOC
             --- Geo ---
-            city: #{data.city}
-            country: #{data.country}
-            lat: #{data.lat}
-            lon: #{data.lon}
-            query: #{data.query}
+            city: Guangzhou
+            country: China
+            lat: 23.1322
+            lon: 113.267
+            query: 183.3.4.1
           HEREDOC
         ).to_stdout
       end
