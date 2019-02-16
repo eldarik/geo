60f54b4a8ae50d33f937bf54a11b52ebcef5de62
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2019-02-16 14:35:32 +0700

    remove unnecessary comments

--- lib/get_geo.rb
+++ lib/get_geo.rb
@@ -6,7 +6,6 @@ require 'get_geo/data_requester'
 
 module GetGeo
   class Error < StandardError; end
-  # Your code goes here...
 
   def self.get_data(ip = nil)
     data = ::GetGeo::DataRequester.execute(ip)
